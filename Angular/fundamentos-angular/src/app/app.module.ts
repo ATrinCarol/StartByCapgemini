134238d9a1d4ede90157772e0cafe1fc3e00f320
Carol Trindade <caroline.alvestrindade@hotmail.com>
2023-01-12 02:29:59 -0300

    interação entre componentes

--- Angular/fundamentos-angular/src/app/app.module.ts
+++ Angular/fundamentos-angular/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TwoWayBidingComponent } from './two-way-biding/two-way-biding.component
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
 import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
+import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { ComponentePersonalizadoComponent } from './componente-personalizado/com
     PipeCustomizadoComponent,
     TwoWayBidingComponent,
     RenderizandoListasComponent,
-    ComponentePersonalizadoComponent
+    ComponentePersonalizadoComponent,
+    ComponentePaiComponent,
+    ComponenteFilhoComponent
   ],
   imports: [
     BrowserModule,
